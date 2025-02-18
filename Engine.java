7012589036593f99ee69f75953ae928793aa663e
yunxiann <yding41@smith.edu>
2025-02-18 12:06:29 -0500

    Finished Engine first version
    Finished Engine first version in class with Ab's instructions

--- Engine.java
+++ Engine.java
@@ -1,3 +1,54 @@
-public class Engine {
+public class Engine implements EngineRequirements{
 
+    //Attributes
+    private FuelType f;
+    private double currentFuelLevel;
+    private double maxFuelLevel;
+
+    /**
+     * Constructor for Engine
+     * @param f Engine's fuel type
+     * @param currentFuelLevel Engine's current fuel level
+     * @param maxFuelLevel Engine's maximum fuel level
+     */
+    public Engine(FuelType f, double currentFuelLevel, double maxFuelLevel){
+        this.f = f;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+    }
+
+    public FuelType getFuelType(){
+        return this.f;
+    }
+
+    public double getCurrentFuel(){
+        return this.currentFuelLevel;
+    }
+
+    public double getMaxFuel(){
+        return this.maxFuelLevel;
+    }
+
+    public void refuel(){
+        this.currentFuelLevel = this.maxFuelLevel;
+    }
+
+    public Boolean go(){
+        if(this.currentFuelLevel > 0){
+            this.currentFuelLevel -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return ("Engine has fuel type:" + this.f + " with current fuel level: " + this.currentFuelLevel + " and maximum fuel level: " + this.maxFuelLevel);
+    }
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 0., 100.);
+        System.out.println(myEngine);
+
+        Engine myOtherEngine = new Engine(FuelType.STEAM, 50., 100.);
+        System.out.println(myOtherEngine);
+    }
 }
