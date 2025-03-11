93113a8af6f43497f2845e973dbcfdf5f8d86c41
yunxiann <yding41@smith.edu>
2025-03-10 23:56:55 -0400

    Deleted unnecessary features
    I wanted to do something fancy but this caused problems. Now I just follow the requirements.

--- Passenger.java
+++ Passenger.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 public class Passenger implements PassengerRequirements {
     
     //Attributes
     private String name;
-    private Car currentCar; // Reference to the car the passenger is currently in
 
     /**
      * Constructor for Passenger
@@ -12,7 +9,6 @@ public class Passenger implements PassengerRequirements {
      */
     public Passenger(String name) {
         this.name = name;
-        this.currentCar = null;
     }
 
     /**
@@ -24,57 +20,27 @@ public class Passenger implements PassengerRequirements {
     }
 
     /**
-     * Interact with the train to board a car or directly board a car
-     * @param train Train to board (can be null if directly boarding a car)
-     * @param scanner Scanner object for user input (can be null if directly boarding a car)
-     * @param car Car to board (can be null if interacting with the train)
+     * Board a car by calling c.addPassenger(this)
+     * @param c Car to board
      */
     @Override
-    public void boardCar(Train train, Scanner scanner, Car car) {
-        if (train != null && scanner != null) {
-            train.welcomeMessage(this);
-            while (true) {
-                int carNumber = train.selectCar(scanner);
-                car = train.getCar(carNumber - 1);
-                if (car != null) {
-                    if (car.addPassenger(this)) {
-                        this.currentCar = car; // Update the current car
-                        System.out.println("Passenger " + this.name + " boarded the train.");
-                        break;
-                    } else {
-                        System.out.println("Do you want to select another car? (Y/N)");
-                        String response = scanner.next();
-                        if (response.equalsIgnoreCase("N")) {
-                            System.out.println("Sorry that we can't board you on our train. See you next time.");
-                            break;
-                        }
-                    }
-                } else {
-                    System.out.println("Invalid car selection.");
-                }
-            }
-        } else if (car != null) {
-            if (car.addPassenger(this)) {
-                this.currentCar = car; // Update the current car
-                System.out.println("Passenger " + this.name + " boarded the car.");
-            } else {
-                System.out.println("This car is full.");
-            }
+    public void boardCar(Car c) {
+        if (c.addPassenger(this)){
+            System.out.println("Passenger " + this.name + " boarded the train.");
+        } else {
+            System.out.println("Passenger " + this.name + " could not board the train.");
+        
         }
     }
 
     /**
-     * Call c.removePassenger(this) to remove the passenger from the car
-     * @param c Car to get off, or null to get off the current car
+     * Get off a car by calling c.removePassenger(this)
+     * @param c Car to get off
      */
     @Override
     public void getOffCar(Car c) {
-        if (c == null) {
-            c = this.currentCar;
-        }
-        if (c != null && c.removePassenger(this)) {
+        if (c.removePassenger(this)){
             System.out.println("Passenger " + this.name + " got off the train.");
-            this.currentCar = null; // Reset the current car
         } else {
             System.out.println("Passenger " + this.name + " could not get off the train.");
         }
