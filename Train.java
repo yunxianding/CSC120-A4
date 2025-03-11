06e03bdf5588ce53d32a604b71818b10756089e4
yunxiann <yding41@smith.edu>
2025-03-10 23:57:53 -0400

    Deleted unnecessary features, changed main accordingly

--- Train.java
+++ Train.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Train implements TrainRequirements{
 
@@ -18,7 +17,7 @@ public class Train implements TrainRequirements{
         this.engine = new Engine(fuelType, fuelCapacity, fuelCapacity);
         this.cars = new ArrayList<Car>();
         for (int i = 0; i < nCars; i++){
-            this.cars.add(new Car(passengerCapacity, this)); // Pass the Train object to the Car constructor
+            this.cars.add(new Car(passengerCapacity));
         } 
     }
 
@@ -88,121 +87,9 @@ public class Train implements TrainRequirements{
     }
 }
 
-    /**
-     * Print the number of seats remaining in each car
-     */
-    public void seatsRemainingInEachCar(){
-        for (int i = 0; i < this.cars.size(); i++){
-            System.out.println("Car " + (i + 1) + " has " + this.cars.get(i).seatsRemaining() + " seats remaining.");
-        }
-    }
-
-    /**
-     * Print a welcome message for a passenger
-     * @param p Passenger to welcome
-     */
-    public void welcomeMessage(Passenger p) {
-        System.out.println("DOO-DOO! Welcome onboard Yunxian's Train, " + p.getName() + "!");
-        System.out.println("There are still " + this.seatsRemaining() + " seats available.");
-        this.seatsRemainingInEachCar();
-    }
-
-    /**
-     * Ask a passenger to select a car
-     * @return the car number the passenger selected
-     */
-    public int selectCar(Scanner scanner) {
-        int carNumber = -1;
-        while (true) {
-            System.out.println("Which car do you want to be in? Please enter the car number:");
-            carNumber = scanner.nextInt();
-            if (carNumber >= 1 && carNumber <= this.cars.size()) {
-                break;
-            } 
-            else {
-                System.out.println("Invalid car number. Please select a valid car number.");
-            }
-        }
-        return carNumber;
-    }
-
-    /**
-     * Print a goodbye message for a passenger
-     * @param p Passenger to say goodbye to
-     */
-    public void goodbyeMessage(Passenger p){
-        System.out.println("Thank you for riding Yunxian's Train, " + p.getName() + "!");
-        System.out.println("We hope you had a pleasant ride!");
-    }
-
-    /**
-     * Refuel the train's engine
-     */
-    public void refuelEngine() {
-        this.engine.refuel();
-        System.out.println("The train's engine has been refueled. Moving on again...");
-    }
-
-    /**
-     * Make the train go, consuming fuel
-     */
-    public void go() {
-        if (this.engine.go()) {
-            System.out.println("The train is moving.");
-        } 
-        else {
-            System.out.println("The train cannot move because it is out of fuel.");
-        }
-    }
-
-    /**
-     * Simulate the journey of the train(from Ford Hall to Neilson Library)
-     */
-    public void startJourney() {
-        int distance = 80; // Distance from Ford Hall to Neilson Library
-        System.out.println("Departing from Ford Hall to Neilson Library (" + distance + " km)...");
-
-        for (int km = 0; km < distance; km++) {
-            if (!this.engine.go()) {
-                System.out.println("The train cannot move because it is out of fuel. Refueling...");
-                this.refuelEngine();
-            }
-        }
-
-        System.out.println("We have arrived at Neilson Library.");
-        System.out.println("Please pack your belongings and prepare to get off!");
-    }
-
-    /**
-     * Board passengers at the start
-     */
-    public void boardPassengers(Scanner scanner, Passenger... passengers) {
-        System.out.println("Passengers are boarding at Ford Hall...");
-        for (Passenger p : passengers) {
-            if (this.seatsRemaining() > 0) {
-                p.boardCar(this, scanner, null);
-            } else {
-                System.out.println("We are so sorry that Yunxian's train is already full, " + p.getName() + "!");
-                System.out.println("We hope to see you next time!");
-            }
-        }
-    }
-
-    /**
-     * Leave passengers at the destination
-     */
-    public void leavePassengers(Passenger... passengers) {
-        System.out.println("Passengers are leaving at Neilson Library...");
-        for (Passenger p : passengers) {
-            p.getOffCar(null);
-        }
-    }
-
-
     //Main method
     public static void main(String[] args) {
         Train train = new Train(FuelType.ELECTRIC, 50.0, 2, 3);
-        Scanner scanner = new Scanner(System.in);
         Passenger p1 = new Passenger("A");
         Passenger p2 = new Passenger("B");
         Passenger p3 = new Passenger("C");
@@ -215,18 +102,30 @@ public class Train implements TrainRequirements{
         train.printManifest();
 
         // Board passengers at the start
-        train.boardPassengers(scanner, p1, p2, p3, p4, p5, p6, p7);
+        p1.boardCar(train.getCar(0));
+        p2.boardCar(train.getCar(0));
+        p3.boardCar(train.getCar(0));
+        p4.boardCar(train.getCar(1));
+        p5.boardCar(train.getCar(1));
+        p6.boardCar(train.getCar(1));
+        p7.boardCar(train.getCar(1)); // This should fail as the car is full
+
+        // Print manifest after boarding
+        train.printManifest();
 
         // Start the journey
-        train.startJourney();
+        train.getEngine().go();
 
         // Leave passengers at the destination
-        train.leavePassengers(p1, p2, p3, p4, p5, p6, p7);
+        p1.getOffCar(train.getCar(0));
+        p2.getOffCar(train.getCar(0));
+        p3.getOffCar(train.getCar(0));
+        p4.getOffCar(train.getCar(1));
+        p5.getOffCar(train.getCar(1));
+        p6.getOffCar(train.getCar(1));
+        p7.getOffCar(train.getCar(1)); // This should fail as the passenger was not on the train
 
         // Final check
         train.printManifest();
-
-        // Close the Scanner
-        scanner.close();
     }
 }
