28b7c133958089ed0eb695c2b06d3b5baf59ed5a
Yunxian Ding <yding41@smith.edu>
2025-03-24 18:25:42 -0400

    Changed back to original
    I just realized that it's mentioned in the requirements that it should be void

--- Passenger.java
+++ Passenger.java
@@ -22,29 +22,26 @@ public class Passenger implements PassengerRequirements {
     /**
      * Board a car by calling c.addPassenger(this)
      * @param c Car to board
-     * @return boarding result message
      */
     @Override
-    public String boardCar(Car c) {
+    public void boardCar(Car c) {
         if (c.addPassenger(this)){
-            return "Passenger " + this.name + " boarded the train.";
+            System.out.println("Passenger " + this.name + " boarded the train.");
         } else {
-            return "Passenger " + this.name + " could not board the train.";
-        
+            System.out.println("Passenger " + this.name + " could not board the train.");
         }
     }
 
     /**
      * Get off a car by calling c.removePassenger(this)
      * @param c Car to get off
-     * @return gettig off result message
      */
     @Override
     public void getOffCar(Car c) {
         if (c.removePassenger(this)){
-            return "Passenger " + this.name + " got off the train.";
+            System.out.println("Passenger " + this.name + " got off the train.");
         } else {
-            return "Passenger " + this.name + " could not get off the train.";
+            System.out.println("Passenger " + this.name + " could not get off the train.");
         }
     }
 }
