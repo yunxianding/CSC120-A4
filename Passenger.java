594966f9fed83d1c0e45bf709318b6bb76622621
Yunxian Ding <yding41@smith.edu>
2025-03-24 18:13:46 -0400

    Changed return type from Void to String for boardCar and getOffCar

--- Passenger.java
+++ Passenger.java
@@ -22,13 +22,14 @@ public class Passenger implements PassengerRequirements {
     /**
      * Board a car by calling c.addPassenger(this)
      * @param c Car to board
+     * @return boarding result message
      */
     @Override
-    public void boardCar(Car c) {
+    public String boardCar(Car c) {
         if (c.addPassenger(this)){
-            System.out.println("Passenger " + this.name + " boarded the train.");
+            return "Passenger " + this.name + " boarded the train.";
         } else {
-            System.out.println("Passenger " + this.name + " could not board the train.");
+            return "Passenger " + this.name + " could not board the train.";
         
         }
     }
@@ -36,13 +37,14 @@ public class Passenger implements PassengerRequirements {
     /**
      * Get off a car by calling c.removePassenger(this)
      * @param c Car to get off
+     * @return gettig off result message
      */
     @Override
     public void getOffCar(Car c) {
         if (c.removePassenger(this)){
-            System.out.println("Passenger " + this.name + " got off the train.");
+            return "Passenger " + this.name + " got off the train.";
         } else {
-            System.out.println("Passenger " + this.name + " could not get off the train.");
+            return "Passenger " + this.name + " could not get off the train.";
         }
     }
 }
